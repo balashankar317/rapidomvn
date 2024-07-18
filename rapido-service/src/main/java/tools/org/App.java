e0106374d7f4d46ebc49e56505c62e59a7d86755
balashankar317 <balashankar9885@gmail.com>
2024-07-18 15:44:04 +0530

    1st

---
+++ rapido-service/src/main/java/tools/org/App.java
@@ -0,0 +1,13 @@
+package tools.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
